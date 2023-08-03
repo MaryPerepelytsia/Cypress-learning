55ef28ac824a8915e31b452acca748470388190f
MaryPerepelytsia <marynkaperepelytsia@gmail.com>
2023-08-03 21:56:02 +0300

    03.08 Change selectors to Data-cy

---
+++ cypress/integration/features/registration.feature
@@ -0,0 +1,8 @@
+Feature: Verification of Bloomenty Registration page
+
+# Scenario: Verify 'Login' button from the Home page
+#     When I navigate to "Home" page
+#     And I wait for "3" seconds
+#     And I press 'Cancel' button on the 'Cookies' page
+#     Then I should see that 'Login' page is displayed
+ 
--- cypress/selectors/loginPage_selectors.js
+++ cypress/selectors/loginPage_selectors.js
@@ -3,10 +3,10 @@
 export default class LoginPage_selectors {
     // name = "selector"
     loginPage = ".login";
-    emailInputField = "#login_username";
-    passwordInputField = "#loginPassword";
-    loginButton = "#login_form_submit";
-    myProfileTitle = ".title-icon > .title";
+    emailInputField = "[data-cy=emailInputField]";
+    passwordInputField = "[data-cy=passwordInputField]";
+    loginButtonOnLoginPage = "[data-cy=loginFormSubmitButton]";
+    myProfileTitle = "[data-top=323.18748781085014]";
     invalidEmailAddressOrPassword = ".alert";
     myProfileAvatar = ".header-user-image > img";
   }
